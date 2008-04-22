d6e5085e812ba17b76b46276b40e2dd1a28b32bf
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-04-22 23:52:04 +0300

    Initial source code import

---
+++ rbmodexcl.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'inline'
+
+class Object
+  inline(:C) do |builder|
+    builder.c %{
+      VALUE exclude(VALUE mod) 
+      {
+        VALUE p, prev;
+        Check_Type(mod, T_MODULE);
+        
+        p = (TYPE(self) == T_CLASS) ? self : rb_singleton_class(self);
+        
+        while (p) {
+            if (p == mod || RCLASS(p)->m_tbl == RCLASS(mod)->m_tbl) {
+                RCLASS(prev)->super = RCLASS(p)->super;
+                rb_clear_cache();
+                return Qtrue;
+            }
+            prev = p;
+            p = RCLASS(p)->super;
+        }
+        return Qfalse;
+      }
+    }
+  end
+
+end
